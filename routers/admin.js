be2260e19aa008c2aa139ffa8a772931da72a70b
dongdeyu <1260129458@qq.com>
2020-09-21 16:24:19 +0800

    后台新增留言板功能增删改 查询复用

--- routers/admin.js
+++ routers/admin.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let User = require("../models/Users")
 let Category = require("../models/Category")
 let Content = require("../models/Content")
+let Comments = require("../models/Comments")
 //统一返回模式
 let responseData;
 //初始化
@@ -438,6 +439,157 @@ router.get("/content/delete", function (req, res) {
             userInfo: req.userInfo,
             message: '删除成功',
             url: "/admin/content"
+        });
+    })
+})
+
+
+/*
+* 留言列表页面
+* */
+
+router.get("/message", function (req, res) {
+    let page = Number(req.query.page || 1);
+    const limit = 10;
+    let pages = 0;
+    //查询数据库的总条数 用count方法
+    Comments.count().then(function (count) {
+        //计算总页数
+        pages = Math.ceil(count / limit);
+        //取值不能超过pages
+        page = Math.min(page, pages);
+        //取值不能小于1
+        page = Math.max(page, 1);
+        let skip = (page - 1) * limit;
+        Comments.find().sort({ _id: -1 }).limit(limit).skip(skip).populate(["category", "user"]).sort({ reviewTime: -1 }).then(function (contents) {
+            res.render("admin/message_index", {
+                contents: contents,
+                count: count,
+                pages: pages,
+                limit: limit,
+                page: page,
+            });
+        });
+    })
+})
+
+/*
+* 留言编辑页面
+* */
+
+router.get("/message/edit", function (req, res) {
+    let id = req.query.id || "";
+    Comments.findOne({ _id: id }).then(function (comment) {
+        if (!comment) {
+            res.render("admin/error", {
+                userInfo: req.userInfo,
+                message: "指定内容不存在"
+            })
+            return Promise.reject()
+        } else {
+            console.log( req.userInfo)
+            console.log(comment)
+            res.render("admin/message_edit", {
+                userInfo: req.userInfo,
+                comment: comment
+            })
+        }
+    })
+
+})
+
+
+/*
+* 留言编辑页面按钮提交事件
+* */
+router.post("/message/edit", function (req, res) {
+    let content = req.body.content || "";
+    let id = req.query.id || "";
+    Comments.findOne({ _id: id }).then(function (comment) {
+        //找不到id
+        if (!comment) {
+            res.render('admin/error', {
+                userInfo: req.userInfo,
+                message: '分类信息不存在'
+            });
+            return Promise.reject();
+        } else {
+            //当用户没有做任何的修改的时候
+            if (content == comment.content) {
+                res.render("admin/success", {
+                    userInfo: req.userInfo,
+                    message: "修改成功",
+                    url: "/admin/category"
+                })
+                return Promise.reject();
+            } else {
+                //要修改的分类名称是否在数据库中存在
+                return Category.findOne({
+                    _id: { $ne: id },
+                })
+            }
+        }
+    }).then(function () {
+        return Comments.update({
+            _id: id,
+          
+        },{
+            content: content
+        });
+    }).then(function () {
+        res.render('admin/success', {
+            userInfo: req.userInfo,
+            message: '修改成功',
+            url: '/admin/message'
+        });
+    })
+})
+
+
+/*
+* 留言增加页面
+* */
+
+router.get("/message/add", function (req, res) {
+    res.render("admin/message_add", {
+        userInfo: req.userInfo,
+    })
+})
+
+/*
+* 留言增加页面保存事件
+* */
+
+router.post("/message/add", function (req, res) {
+    if (req.body.username == "") {
+        res.json(200, { code: 20000, error: '请填写内容' })
+        return false
+    }
+    new Comments({
+        username: req.userInfo.username,
+        reviewTime: new Date(),
+        content: req.body.content
+
+    }).save().then(function (rs) {
+        res.render('admin/success', {
+            userInfo: req.userInfo,
+            message: '修改成功',
+            url: '/admin/message'
+        });
+    });
+})
+
+/*
+* 留言页面删除事件
+* */
+
+router.get("/message/delete", function (req, res) {
+    let id = req.query.id || "";
+    Comments.remove({ _id: id }).then(function () {
+        res.render('admin/success', {
+            userInfo: req.userInfo,
+            message: '删除成功',
+            url: "/admin/message"
         });
     })
 })
