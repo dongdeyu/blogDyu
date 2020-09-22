19b091ced10f077c918d042704a088c1d27410ca
dongdeyu <1260129458@qq.com>
2020-09-22 17:34:59 +0800

    changes

--- public/js/mine.js
+++ public/js/mine.js
@@ -9,29 +9,31 @@ $(function () {
         tabsize: 2,
         height: 100
     });
-    $("#summernoteBtn").click(function () {
-        var code = $("#summernote").summernote("code");
-        $.ajax({
-            type: "post",
-            url: "/admin/content/add",
-            data: {
-                title: $("#title").val(),
-                description: $("#description").val(),
-                content: $("#content").val(),
-                category: $("#category option:selected")[0].value,
-                codeCont:code
-            },
-            dataType: 'json',
-            success: function (result) {
-                if (result.code == 20000) {
-                    console.log(result.error)
-                    layer.msg(result.error, function () {});
-                }
-            },
-            error: function (error) {
-                console.log(error)
-            }
-        })
+    $("#sumButton").click(function () {
+        var content1 = $("#summernote2").summernote("code");
+        var content2 = $("#summernote3").summernote("code");
+        console.log($("#image").val())
+        // $.ajax({
+        //     type: "post",
+        //     url: "/admin/mine/add",
+        //     data: {
+        //         title: $("#title").val(),
+        //         description: $("#description").val(),
+        //         content: $("#content").val(),
+        //         category: $("#category option:selected")[0].value,
+        //         codeCont:code
+        //     },
+        //     dataType: 'json',
+        //     success: function (result) {
+        //         if (result.code == 20000) {
+        //             console.log(result.error)
+        //             layer.msg(result.error, function () {});
+        //         }
+        //     },
+        //     error: function (error) {
+        //         console.log(error)
+        //     }
+        // })
 
     })
 
