e14394113504d007a284b7dd8bfb0a160e72cfff
dongdeyu <1260129458@qq.com>
2020-09-23 13:59:24 +0800

    oss 上传增加

--- public/js/mine.js
+++ public/js/mine.js
@@ -1,40 +1,67 @@
-$(function () {
-    $('#summernote2').summernote({
-        placeholder: 'Hello Bootstrap 4',
-        tabsize: 2,
-        height: 100
-    });
-    $('#summernote3').summernote({
-        placeholder: 'Hello Bootstrap 4',
-        tabsize: 2,
-        height: 100
-    });
-    $("#sumButton").click(function () {
-        var content1 = $("#summernote2").summernote("code");
-        var content2 = $("#summernote3").summernote("code");
-        console.log($("#image").val())
-        // $.ajax({
-        //     type: "post",
-        //     url: "/admin/mine/add",
-        //     data: {
-        //         title: $("#title").val(),
-        //         description: $("#description").val(),
-        //         content: $("#content").val(),
-        //         category: $("#category option:selected")[0].value,
-        //         codeCont:code
-        //     },
-        //     dataType: 'json',
-        //     success: function (result) {
-        //         if (result.code == 20000) {
-        //             console.log(result.error)
-        //             layer.msg(result.error, function () {});
-        //         }
-        //     },
-        //     error: function (error) {
-        //         console.log(error)
-        //     }
-        // })
 
-    })
+var url = ''
+$('#summernote2').summernote({
+    placeholder: 'Hello Bootstrap 4',
+    tabsize: 2,
+    height: 100
+});
+$('#summernote3').summernote({
+    placeholder: 'Hello Bootstrap 4',
+    tabsize: 2,
+    height: 100
+});
+
+var client = new OSS.Wrapper({
+    region: 'oss-cn-beijing',
+    accessKeyId: 'LTAI4G1JtCSQop12y2EFAjiv',
+    accessKeySecret: 'gGg3RYltVfDF2UAsijIdtcCq0a9Mij',
+    bucket: 'dongdeyu',
+    secure: true
+});
+
+function uploadPic(obj) {
+    var file = obj.files[0];//获取文件流
+    var val = obj.value;
+    var suffix = val.substr(val.indexOf("."));
+    var storeAs = 'blog/' + timestamp() + suffix;
+    client.multipartUpload(storeAs, file).then(function (result) {
+        var options = { expires: 10000000 }; //options可以传入链接的失效时间
+        url = client.signatureUrl(result.name, options);
+        console.log(url)
+        // 获取url后直接走接口将url传给node 保存在数据库中        
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+console.log($("#sumButton"))
+$("#sumButton").click(function () {
+    console.log(111)
+    var content1 = $("#summernote2").summernote("code");
+    var content2 = $("#summernote3").summernote("code");
+    console.log(content1)
+    console.log(url)
+
+    // $.ajax({
+    //     type: "post",
+    //     url: "/admin/mine/add",
+    //     data: {
+    //         title: $("#title").val(),
+    //         description: $("#description").val(),
+    //         content: $("#content").val(),
+    //         category: $("#category option:selected")[0].value,
+    //         codeCont:code
+    //     },
+    //     dataType: 'json',
+    //     success: function (result) {
+    //         if (result.code == 20000) {
+    //             console.log(result.error)
+    //             layer.msg(result.error, function () {});
+    //         }
+    //     },
+    //     error: function (error) {
+    //         console.log(error)
+    //     }
+    // })
 
 })
--- public/js/utils.js
+++ public/js/utils.js
@@ -5,3 +5,23 @@ function formatDate2(d) {
     let seconds = date1.getSeconds()>9?date1.getSeconds():'0'+date1.getSeconds()
     return date1.getFullYear() + '年' + (date1.getMonth()+1) + '月' + date1.getDate() + '日 ' + hours + ':' + minutes + ':' + seconds;
 }
+
+/**
+ * 生成文件名
+ * @returns
+ */
+function timestamp() {
+    var time = new Date();
+    var y = time.getFullYear();
+    var m = time.getMonth() + 1;
+    var d = time.getDate();
+    var h = time.getHours();
+    var mm = time.getMinutes();
+    var s = time.getSeconds();
+    console.log(y);
+    return "" + y + add0(m) + add0(d) + add0(h) + add0(mm) + add0(s);
+}
+function add0(m) {
+    return m < 10 ? '0' + m : m;
+}
+
